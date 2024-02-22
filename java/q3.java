ccff9cad5339bb885af42f99dcb29d58d905b3fd
Rahul pt <146359399+Rahulpt002@users.noreply.github.com>
2024-02-22 21:20:40 +0530

    Update q3.java

--- java/q3.java
+++ java/q3.java
@@ -1,7 +1,24 @@
 public class q3 {
     public static void main(String[] args) 
     {
-        int num=
+        int num=153;
+        int amg=0;
+        int chk=num;
+        while(num !=0)
+        {
+            int n = num % 10;
+            num = num/10;
+            n=n*n*n;
+            amg=amg+n;
+        }
+        if(chk==amg)
+        {
+            System.out.println("is amgstrong");
+        }
+        else
+        {
+            System.out.println("is not amgstrong");
+        }
     }
     
 }
