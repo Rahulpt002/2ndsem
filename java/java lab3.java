73492bc6322dc01d917d83075a5781dafb78e7f3
Rahul pt <146359399+Rahulpt002@users.noreply.github.com>
2024-03-06 15:27:03 +0530

    Create java lab3.java

---
+++ java/java lab3.java
@@ -0,0 +1,14 @@
+public class math
+{
+public static void main(String args[])
+{
+int[]arr;
+int i;
+for(i=1;i<=10;i++)
+{
+arr=new int[11];
+arr[i]=2*i;
+System.out.println("2"+""+"*"+i+""+"="+arr[i]);
+}
+}
+}
